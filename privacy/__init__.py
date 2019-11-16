7535601cc0b03991f903d8f0466df5d5bffd35df
Luke <luke@lmbyrne.dev>
2019-11-16 17:34:54 +0000

    Add should_backoff for handling 429 and 5xx retries.

--- privacy/__init__.py
+++ privacy/__init__.py
@@ -1,2 +1,7 @@
 """A privacy.com API wrapper."""
 from privacy.api_client import APIClient as Client
+
+client = Client("5923e5cf-cb3f-473c-90d3-2bfa8304ae36", sandboxed=True)
+cards = client.cards_list(page_size=1)
+for card in cards:
+    print(card)
